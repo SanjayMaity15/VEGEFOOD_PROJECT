7aed4241a237c6d790c8c2628d1e78ad52a904f4
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2025-07-02 21:29:36 +0530

    header complete

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-import './style.css';
+import "./style.css";
+
+// Active and close mobile menu
+
+const humburger = document.getElementById("humburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeMenu = document.getElementById('close-menu');
+
+humburger.addEventListener("click", () => {
+  mobileMenu.classList.add("mobile-menu-active");
+  humburger.style.display = "none";
+  closeMenu.style.display = "block";
+  humburger.classList.remove('humburger-active')
+  
+});
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("mobile-menu-active");
+  humburger.classList.add('humburger-active');
+  closeMenu.style.display = "none";
+  
+});
