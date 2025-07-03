cca2a96bffd4928c63dff74ef0cdae83e8b4425c
SanjayMaity15 <sanjaymaityofficial15@gmail.com>
2025-07-03 11:15:42 +0530

    hero-section complete

--- src/main.js
+++ src/main.js
@@ -4,18 +4,37 @@ import "./style.css";
 
 const humburger = document.getElementById("humburger");
 const mobileMenu = document.querySelector(".mobile-menu");
-const closeMenu = document.getElementById('close-menu');
+const closeMenu = document.getElementById("close-menu");
 
 humburger.addEventListener("click", () => {
   mobileMenu.classList.add("mobile-menu-active");
   humburger.style.display = "none";
   closeMenu.style.display = "block";
-  humburger.classList.remove('humburger-active')
-  
+  humburger.classList.remove("humburger-active");
 });
 closeMenu.addEventListener("click", () => {
   mobileMenu.classList.remove("mobile-menu-active");
-  humburger.classList.add('humburger-active');
+  humburger.classList.add("humburger-active");
   closeMenu.style.display = "none";
-  
 });
+
+// Hero section bg image crousal
+
+const bgHero = document.querySelectorAll(".bg-hero");
+let currnt = 0;
+
+function removeBgActiveClass() {
+  bgHero.forEach((currBg) => {
+    currBg.classList.remove("hero-bg-active");
+  });
+}
+
+function showNextBg() {
+  removeBgActiveClass();
+  currnt = (currnt + 1) % bgHero.length;
+  bgHero[currnt].classList.add("hero-bg-active");
+}
+
+setInterval(() => {
+  showNextBg();
+}, 5000);
